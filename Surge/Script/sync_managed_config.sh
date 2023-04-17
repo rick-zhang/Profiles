29ca3da69bfc6eaf40254426c45320ed93cf0651
张幸锋 <rick.zhang@chiefclouds.com>
2023-04-17 18:34:34 +0800

    添加测试Module

---
+++ Surge/Script/sync_managed_config.sh
@@ -0,0 +1,2 @@
+curl -k https://sub.nexconvert.com/downloadConfig/SurgeConfig.aspx?urk=75b67a09-803e-433e-b3c1-0abf7870febc -o /Users/rickzhang/Library/Application\ Support/Surge/Profiles/Nexitally_Surge_Local.conf && sed -i '' '1d' /Users/rickzhang/Library/Application\ Support/Surge/Profiles/Nexitally_Surge_Local.conf
+
---
+++ Surge/Script/us_group.js
@@ -0,0 +1,19 @@
+const url = 'https://sub.nexconvert.com/downloadConfig/NodeList.aspx?urk=75b67a09-803e-433e-b3c1-0abf7870febc';
+const usNodes = [];
+
+$httpClient.get(url, function (error, response, data) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const nodeList = data.split('\n');
+  usNodes.length = 0;
+  for (let node of nodeList) {
+    if (node.startsWith('🇺🇸 USA')) {
+      usNodes.push(node);
+    }
+  }
+  $surge.setSelectGroupPolicy('US', 'url-test', usNodes);
+});
+
+$done();
